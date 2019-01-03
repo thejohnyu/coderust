a239745b75eee885d2c47a06512d3a05dfc19b11
John Yu <johnyu.programmer@gmail.com>
2019-01-03 04:35:23 -0800

    feat: addToHead method

--- Linked-list/Create/ex2.js
+++ Linked-list/Create/ex2.js
@@ -7,3 +7,15 @@ function Node(value, next, prev) {
   this.next = next;
   this.prev = prev;
 }
+
+LinkedList.prototype.addToHead = function(value) {
+  const newNode = new Node(value, this.head, null);
+  if (this.head) this.head.prev = newNode;
+  else this.tail = newNode; 
+  this.head = newNode;
+};
+
+const list = new LinkedList();
+list.addToHead(100);
+list.addToHead(200);
+console.log(list);
