28838820d34eacccbfe5297dc04338c6263ae8b8
John Yu <johnyu.programmer@gmail.com>
2019-01-04 00:13:23 -0800

    FEAT: add single linked list

--- linkedlist/Create/ex2.js
+++ linkedlist/Create/ex2.js
@@ -15,7 +15,27 @@ LinkedList.prototype.addToHead = function(value) {
   this.head = newNode;
 };
 
+LinkedList.prototype.addToTail = function(value) {
+  const newNode = new Node(value, null, this.tail);
+  if (this.tail) this.tail.next = newNode;
+  else this.head = newNode;
+  this.tail = newNode;
+}
+
+// const list = new LinkedList();
+// list.addToHead(100);
+// list.addToHead(200);
+// console.log(list);
+
+// const otherlist = new LinkedList();
+// otherlist.addToHead(100);
+// otherlist.addToHead(200);
+// otherlist.addToHead(300);
+// console.log(otherlist);
+// console.log(`Middle node value: ${otherlist.head.next.value}`);
+
 const list = new LinkedList();
-list.addToHead(100);
-list.addToHead(200);
-console.log(list);
+list.addToTail(100);
+list.addToTail(200);
+list.addToTail(300);
+console.log('list', list);
