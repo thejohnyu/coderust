607236080a9b448301b5f8210944064616fa86d9
thejohnyu <johnyu.programmer@gmail.com>
2019-12-22 16:01:30 -0500

    more practice

--- math/Is Number Valid/ex1.js
+++ math/Is Number Valid/ex1.js
@@ -1,64 +1,80 @@
 const STATE = {
-    INTEGER: 'INTEGER',
-    DECIMAL: 'DECIMAL',
-    START: 'START',
-    UNKNOWN: 'UNKNOWN',
-    AFTER_DECIMAL: 'AFTER_DECIMAL'
+  INTEGER: "INTEGER",
+  DECIMAL: "DECIMAL",
+  START: "START",
+  UNKNOWN: "UNKNOWN",
+  AFTER_DECIMAL: "AFTER_DECIMAL"
 };
 
-let get_next_state = function(current_state, ch) {
-    if (current_state === STATE.START ||
-        current_state === STATE.INTEGER) {
-        if (ch === '.') {
-            return STATE.DECIMAL;
-        } else if (ch >= '0' && ch <= '9') {
-            return STATE.INTEGER;
-        } else {
-            return STATE.UNKNOWN;
-        }
+let get_next_state = function(current_state, ch) {// ('start', '4')
+  const isInterger = ch >= "0" && ch <= "9";
+  const isDecimal = ch === ".";
+
+  if (current_state === STATE.START || current_state === STATE.INTEGER) {
+    if (isDecimal) {
+      return STATE.DECIMAL;
+    } else if (isInterger) {
+      // make sure the number is not 2 digits.
+      return STATE.INTEGER;
+    } else {
+      return STATE.UNKNOWN;
     }
-    if (current_state === STATE.DECIMAL) {
-        if (ch >= '0' && ch <= '9') {
-            return STATE.AFTER_DECIMAL;
-        } else {
-            return STATE.UNKNOWN;
-        }
+  }
+  if (current_state === STATE.DECIMAL) {
+    if (isInterger) {
+      // this ensures that the next number is an interger
+      return STATE.AFTER_DECIMAL;
+    } else {
+      return STATE.UNKNOWN;
     }
-    if (current_state === STATE.AFTER_DECIMAL) {
-        if (ch >= '0' && ch <= '9') {
-            return STATE.AFTER_DECIMAL;
-        } else {
-            return STATE.UNKNOWN;
-        }
+  }
+  if (current_state === STATE.AFTER_DECIMAL) {
+    if (isInterger) {
+      return STATE.AFTER_DECIMAL;
+    } else {
+      return STATE.UNKNOWN;
     }
-    return STATE.UNKNOWN;
+  }
+  return STATE.UNKNOWN;
 };
+
+ // At a high level: we will split each string and iterate over each element.
+ // As we iterate over 
 
 let is_number_valid = function(s) {
-    if (s.length === 0) {
-        return true;
-    }
 
-    let i = 0;
-    if (s[i] === '+' || s[i] === '-') {
-        i++;
-    }
-
-
-    let current_state = STATE.START;
-
-    for (let l = i; l < s.length; l++) {
-        current_state = get_next_state(current_state, s[l]);
-        if (current_state === STATE.UNKNOWN) {
-            return false;
-        }
-    }
-  
-    if (current_state === STATE.DECIMAL)
-      return false;
-  
+  // "4.325"
+  if (s.length === 0) {
     return true;
+  }
+
+  let i = 0;
+  if (s[i] === "+" || s[i] === "-") {
+    // check for positive / negative operator... this is edge case
+    i++;
+  }
+
+  let current_state = STATE.START; // acc
+
+  for (let l = i; l < s.length; l++) {
+    // We iterate over the string ['4', '.', '3', '2', '5']
+    current_state = get_next_state(current_state, s[l]); // ('start', '4')
+    if (current_state === STATE.UNKNOWN) {
+      return false;
+    }
+  }
+
+  if (current_state === STATE.DECIMAL) return false;
+
+  return true;
 };
+// Constraits ---> decimals are valid, negative, positive operators are valid. everything else is false
+// Input = "4.325"
+// 1. STATE.START
+// 2. STATE.INTEGER
+// 3. STATE.DECIMAL
+// 4. STATE.AFTER_DECIMAL
+// 5.
 
 console.log("");
 console.log("");
@@ -80,3 +96,63 @@ console.log('is this number valid "+1.1" = ' + is_number_valid("+1.1"));
 console.log('is this number valid "-1.1." = ' + is_number_valid("-1.1."));
 console.log('is this number valid "-1.1" = ' + is_number_valid("-1.1"));
 console.log('is this number valid "" = ' + is_number_valid(""));
+
+const getNextState = (current_state, character) => {
+    const state = {
+        'START': 'START',
+        'DECIMAL': 'DECIMAL',
+        'AFTER_DECIMAL': 'AFTER_DECIMAL',
+        'INTERGER': 'INTERGER',
+        'UNKNOWN' : 'UNKNOWN'
+    }
+
+    if (current_state === state.START || current_state === state.INTERGER) {
+        if (character === '.') {
+            return state.DECIMAL
+        } else if ( character > 0 && character < 9) {
+            return state.INTERGER
+        } else {
+            return state.UNKNOWN
+        }
+    }
+
+    if (current_state === state.DECIMAL){
+        if (character > 0 && character < 9) {
+            return state.INTERGER
+        } else {
+            return state.UNKNOWN
+        }
+    }
+
+    if (current_state === state.AFTER_DECIMAL) {
+        if (character === '.') {
+            return state.DECIMAL
+        } else if ( character > 0 && character < 9) {
+            return state.INTERGER
+        } else {
+            return state.UNKNOWN
+        }
+    }
+
+    return state.UNKNOWN
+}
+const isNumberValid = s => {
+    if (s.length === 0) {
+        return true
+    }
+
+    let currentState = null
+
+    for (let i = 0; i < s.length; i++) {
+        currentState = getNextState(currentState, s[i])
+        if (currentState === 'UNKNOWN') {
+            return false
+        }
+    }
+
+    if (currentState === 'DECIMAL') {
+        return false
+    }
+
+    return true
+}
